86d7ee7f824bed96f498534944351523e9c63267
danielaandrademendes-rgb <danielaandrademendes@gmail.com>
2025-12-25 18:18:47 +0000

    aplicando matrizes e vetores

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,51 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int i, j,k, l;
+    char cordenadas[10]= {'A','B','C','D','E','F','G','H','I','J'}; //vetor para armazenar as letras do cabeçalho do tabuleiro
+    int tabuleiro[10][10]={
+        {1,0,0,0,0,0,0,0,0,0}, // matriz 10x10 para representar o tabuleiro
+        {2,0,0,0,0,0,0,0,0,0},
+        {3,0,0,0,0,0,0,0,0,0},
+        {4,0,0,0,0,0,0,0,0,0},
+        {5,0,0,0,0,0,0,0,0,0},
+        {6,0,0,0,0,0,0,0,0,0},
+        {7,0,0,0,0,0,0,0,0,0},
+        {8,0,0,0,0,0,0,0,0,0},
+        {9,0,0,0,0,0,0,0,0,0},
+        {10,0,0,0,0,0,0,0,0,0},
+    
+    };
+    printf("TABULEIRO BATALHA NAVAL\n");  // imprime a string no console
+
+    for(l = 3; l < 6; l ++){  //loop para fazer o posicionamento dos navios 
+        tabuleiro[2][l] = 3; // Navio Horizontal: tabuleiro na linha 2 e colunas de 3 a 5 trocado o valor para 3 
+    }
+    for(l = 3; l < 6 ; l ++){ //loop para fazer o posicionamento dos navios
+        tabuleiro[l][7] = 3; // Navio Vertical
+    }                      //tabuleiro na coluna 7 e linhas de 3 a 5 trocado o valor para 3 
+
+     printf("  "); // Espaço inicial para alinhar as letras do cabeçalho com a matriz
+
+    for(i=0; i<10; i++){ //loop para imprimir as letras do cabeçalho
+        printf("%2c",cordenadas[i]); //imprime os caracteres de acordo com o indice com o valor de i
+    }                                  //exemplo: quando i = 0, imprime A, quando i = 1, imprime B, e assim por diante
+
+
+    printf("\n"); // pula uma linha após imprimir as letras do cabeçalho
+
+    for(j = 0; j < 10; j++){ //loop for aninhado para imprimir a matriz
+        for(k = 0; k < 10; k++){ //cada vez que o primeiro loop roda, o segundo roda completamente 
+            printf("%2d", tabuleiro[j][k]);// imprime o valor da matriz na posiçao [j][k]
+        }                                   //exemplo: quando j = 0, k roda de 0 a 9, imprimindo toda a primeira linha
+        printf("\n");                       //%2d saida com 2 espaços para cada numero
+    }
+
+
+
+
+
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
